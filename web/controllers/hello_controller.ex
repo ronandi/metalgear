83939b9d599002c8cc4c04192b3f1f61388338b3
Rajan Singh <rajan1992@hotmail.com>
2015-01-21 23:33:17 -0800

    failed to make basic routes

---
+++ web/controllers/hello_controller.ex
@@ -0,0 +1,10 @@
+defmodule HelloPhoenix.HelloController do
+	use Phoenix.Controller
+	
+	plug :action
+
+	def index(conn, _params) do
+		render conn, "index.html"
+	end
+end
+	
--- web/router.ex
+++ web/router.ex
@@ -16,6 +16,7 @@ defmodule Vgdb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/hello", HelloController, :index
   end
 
   # Other scopes may use custom stacks.
